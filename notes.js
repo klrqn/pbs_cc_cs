8edad73b5e48a1baaf8e05a7a0ebd0f29d318ebe
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-07-03 13:20:12 +0000

    ep1-2

---
+++ notes.js
@@ -0,0 +1,36 @@
+"Computer"
+was originally a person who computed!
+
+    Relay - an electrical device, typically incorporating an electromagnet, which is activated by a current or signal in one circuit to open
+or close another circuit.
+
+Harvard Mark 1 - Conceived by Harvard physics professor Howard Aiken, and designed and built by IBM, the Harvard Mark 1 is a room - sized,
+    relay - based calculator.The machine had a fifty - foot long camshaft running the length of machine that synchronized
+    the machine’ s thousands of component parts and used 3, 500 relays .1944
+
+    1947 - A moth was pulled from the Mark 1 "From then on, when anything went wrong with a computer, we said it had bugs in it" - Grace Hopper
+
+"VACUUM TUBES" -
+    "Diode" - Any electronic component that allows the one - way transmission of current
+    Thermionic Valve - First Vacuum Tube - Thermionic Transmission - Fleming -
+    "Vacuum Tubes" are like relays with one "GREAT" Exception - they have no moving parts.
+    They are, however, fragile and can burn - out like lightbulbs, but they are a major improvement over mechanical relays
+    They marked the transition from ELECTROMECHANICAL computing to ELECTRONIC Computing
+
+"Colossus Mark 1" - First programmable, electronic computer. Built to decode Nazi Encryption
+    Programming was done by plugging hundreds of wires into "Plugboards", sort of like old school telephone switchboards,
+    in order to set up the computer to perform the right operation
+
+"ENIAC" - first truly general purpose computer
+===================================================
+1947 - Bell Labs - "TRANSISTOR" was invented! physics behind transistors relies on quantum physics but the basics are:
+    Transistor is just like a relay or vacuum tube. a switch that can be open or closed by applying electricity via a control wire.
+    Transistors are Solid State!
+
+    Today, computers use transistors that are smaller than 15 nanometers in size. (a sheet of paper is roughly 100,000 nanometers thick)
+    They can switch states millions of times per second, and can run for decades.
+
+===(Episode #3)===
+
+"on" state - when electricity is flowing     = true
+"off" state = when no electricity is flowing = false
